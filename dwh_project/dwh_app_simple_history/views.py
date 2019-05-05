1f0b41ba53da038d1d3d83e4d31fd0e47fd7d245
Arnaud <arnaud@hokodo.co>
2019-05-05 22:52:46 +0100

    Add view endpoint to add person and vehicle
    - Create views.py with one POST method PersonVehicle
    - Update urls to expose the endpoint

--- dwh_project/dwh_app_simple_history/views.py
+++ dwh_project/dwh_app_simple_history/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+from dwh_app_simple_history.serializers import PersonVehicleSerializer
+
+
+@api_view(["POST"])
+def PersonVehicle(request):
+    """
+    This view will be called through a POST request to add or update the information provided in
+    the request
+    """
+
+    # Deserialize the information provided in the request
+    ser = PersonVehicleSerializer(data=request.data)
+
+    # Validate the information provided
+    ser.is_valid(raise_exception=True)
+
+    # Save the information in the datawarehouse
+    ser.save()
+
+    return Response({"All good, everything has been saved"})
--- dwh_project/dwh_project/urls.py
+++ dwh_project/dwh_project/urls.py
@@ -14,8 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
+from dwh_app_simple_history.views import PersonVehicle
+
+urlpatterns = [path("admin/", admin.site.urls), re_path("^PersonVehicle/add/?", PersonVehicle)]
